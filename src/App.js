0cd1b2c02942515e499cd2956f42cc041f288e41
sunhao <sunhaokk@qq.com>
2020-07-07 21:45:34 +0800

    c

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from 'react';
 
-function App() {
+import Menu, { SubMenu, MenuItem } from 'rc-menu';
+import './menu.less'
+export default ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Menu>
+    <MenuItem>1</MenuItem>
+    <SubMenu title="2">
+      <MenuItem>2-1</MenuItem>
+    </SubMenu>
+  </Menu>
+  )
 }
 
-export default App;
+
